5690079cf00a67855f78af48250f7f9da1386e80
Alex McIvor <alexmcivor@gmail.com>
2017-03-02 00:10:42 +0000

    Exits app with a code 0 if the predicate matches, otherwise exits with a code 1. Requires at least 1 matching element to exit with code 0.

---
+++ src/System.Linq/src/System/Linq/Exit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace System.Linq.System.Linq
+{
+    public static partial class Enumerable
+    {
+        public static void Exit<TSource>(IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            if (predicate == null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            foreach(TSource element in source)
+            {
+                if (predicate(element) == true)
+                    Environment.Exit(0);
+            }
+
+            Environment.Exit(1);
+        }
+    }
+}
